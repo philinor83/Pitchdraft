0a1ab9fb748d1c29e31614b3fa43f4255ceb54e7
philinor83 <philipmnorton@icloud.com>
2026-06-29 21:12:03 -0400

    summary

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,11 @@
-{
-  "extends": ["next/core-web-vitals", "next/typescript"]
-}
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  baseDirectory: dirname(fileURLToPath(import.meta.url)),
+});
+
+const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
+
+export default eslintConfig;
